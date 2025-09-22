11ab9d3bae80eb7716ba3876a8e888e5c2840467
Abhinaba Jash <abhinaba@itobuz.com>
2025-09-22 10:36:26 +0530

    feat: Added the subscribe container form validation using js form validation.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
-import '../scss/styles.scss'
+import "../scss/styles.scss";
 
 // Import all of Bootstrap’s JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
+const form = document.getElementById("subscribe-form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  try {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    if (name === "") {
+      console.log({ error: "Name field can't be empty." });
+      return;
+    } else if (email === "") {
+      console.log({ error: "Email can't be empty." });
+      return;
+    }
+    const formData = { state: "success", data: { name, email } };
+    console.log(formData);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
